b49a15655cee4e0aecf025301ab7118a941b5f9f
rogerramosruiz <rogerramosruizdev@gmail.com>
2023-03-24 11:12:11 -0400

    db initialiazation for task

--- task/src/app.py
+++ task/src/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
-from config.environment import PORT, DEBUG
 from routes.task import task
+
+from config.environment import PORT, DEBUG
+from db.init import intialize
 
 app = Flask(__name__)
 app.register_blueprint(task)
 
 
 if __name__ == '__main__':
+    intialize()
     app.run('0.0.0.0', PORT, DEBUG)
--- task/src/config/environment.py
+++ task/src/config/environment.py
@@ -10,5 +10,6 @@ DB_NAME = os.getenv('DB_NAME')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_PORT = os.getenv('DB_PORT')
 DB_USER = os.getenv('DB_USER')
+DB_INIT_SCRIPT_PATH = os.getenv('DB_INIT_SCRIPT_PATH')
 
 AUTH_SERVER = os.getenv('AUTH_SERVER')
