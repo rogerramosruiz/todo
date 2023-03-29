b22cdde172971dfa18ab9f450417b8dd4bcd250d
rogerramosruiz <rogerramosruizdev@gmail.com>
2023-03-29 09:20:18 -0400

    create task and taskStore

---
+++ frontend/src/lib/components/Task.svelte
@@ -0,0 +1,30 @@
+<script>
+    import {taskStore} from '../../lib/store/taskStore'
+
+    export let task
+    
+    let edit = false
+    function save(){
+        edit = false
+    }
+    
+</script>
+
+<div>
+    {#if !edit}
+        {task.name}
+        {task.done}
+        <button on:click={()=>edit=true}>
+            edit
+        </button>
+        {:else}
+        <input value={task.name} class="">
+        <button on:click={save}>
+            save
+        </button>
+
+    {/if}
+        <button>
+            delete
+        </button>
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script>
+    import {taskStore} from '../lib/store/taskStore'
     import {onMount} from "svelte"
     import { goto } from '$app/navigation'
     import {request} from '../lib/request/request'
     import Task from "../lib/components/Task.svelte";
 
+    let name = ''
 
-    let tasks = []
-    
     onMount(async () => {
         if(localStorage.accessToken == null || localStorage.refreshToken == null){
             goto('login')
@@ -14,17 +14,49 @@
         }
         const resp = await request('api/v1/task', true)
         if (resp.status === 200)
-            tasks = await resp.json()
-        })
+            $taskStore = await resp.json()
+     })
 
+     async function createTask(){
+        if (name == ''){
+            console.log('tasname emtpy')
+            return
+        }
+        let body = {
+            name
+        }
+        const resp = await request('api/v1/task', true, 'POST', body)
+        const data = await resp.json()
+        if(resp.status === 201){
+            $taskStore = [{
+                id: data.id,
+                done: false,
+                name: name
+            },
+            ...$taskStore
 
+        ]
+        }
 
+        name = ''
+    }
 
+     
 </script>
 
 <main>
+    <form on:submit|preventDefault={createTask}>
+        <h1>Please sign in</h1>
+        <div>
+            <label for="name">Task name</label>
+            <input id="name" type="text" bind:value ={name} placeholder="task name">
+        </div>
+        <button type="submit">Submit</button>
+    </form>
+
+
     <h1>Tasks</h1>    
-    {#each tasks as task}
+    {#each $taskStore as task}
 		<li>
             <Task task={task}/>
 		</li>
