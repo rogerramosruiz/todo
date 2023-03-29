45eb77e9d19697b84012906e705b310cf5c8dd74
rogerramosruiz <rogerramosruizdev@gmail.com>
2023-03-29 09:26:13 -0400

    delete task

--- frontend/src/lib/components/Task.svelte
+++ frontend/src/lib/components/Task.svelte
@@ -1,11 +1,21 @@
 <script>
     import {taskStore} from '../../lib/store/taskStore'
-
+    import {request } from '../request/request'
     export let task
     
     let edit = false
     function save(){
         edit = false
+    }
+    async function remove(){
+    const resp = await request(`api/v1/task/${task.id}`, true, 'DELETE')
+    if (resp.status == 202){
+        console.log('deleted')
+        $taskStore = $taskStore.filter((t)=> t.id !== task.id)
+    }
+    else{
+        console.log(resp)
+    }
     }
     
 </script>
@@ -18,13 +28,13 @@
             edit
         </button>
         {:else}
-        <input value={task.name} class="">
+        <input value={task.name}>
         <button on:click={save}>
             save
         </button>
 
     {/if}
-        <button>
+        <button on:click={remove}>
             delete
         </button>
 </div>
