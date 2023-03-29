caa8ae22cce0cc96149413a27e9c57531972349c
rogerramosruiz <rogerramosruizdev@gmail.com>
2023-03-29 13:16:35 -0400

    login and sign up styled

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -16,17 +16,25 @@
     }
 </script>
 
-<main>
-    <form on:submit|preventDefault={submit}>
-        <h1>Login</h1>
-        <div>
-            <label for="username">Username</label>
-            <input id="username" type="text" bind:value ={username} placeholder="username">
-        </div>
-        <div>
-            <label for="password">Password</label>
-            <input id="password" type="password" bind:value = {password} placeholder="Password">
-        </div>
-        <button type="submit">Submit</button>
-    </form>
-</main>
+<div class="grid place-items-center sm:h-4 w-screen">
+  <form on:submit|preventDefault={submit} class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 lg:w-1/4 ">
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+        Username
+      </label>
+      <input bind:value={username} class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+    </div>
+    <div class="mb-6">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+        Password
+      </label>
+      <input bind:value = {password} class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+      <!-- <p class="text-red-500 text-xs italic">Please choose a password.</p> -->
+    </div>
+    <div class="grid place-items-center">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+        Login
+      </button>
+    </div>
+  </form>
+</div>
--- frontend/src/routes/register/+page.svelte
+++ frontend/src/routes/register/+page.svelte
@@ -3,9 +3,11 @@
     import {request} from '../../lib/request/request'
     
     let username="", password = "", confirmation=""
+    let error = false
     let message = ""
 
     $: submit = async() =>{
+        console.log('requesting ')
         const response = await request('api/v1/auth/signup', false, 'POST', {
             username:username,
             password:password,
@@ -14,35 +16,49 @@
         const data = await response.json()
         if(response.status == 201){
             message = 'user created, redirecting to login'
-            setTimeout(() =>goto('login'), 2000) 
+            setTimeout(() =>goto('login'), 1000) 
+            error = false
         }
         else{
+            error = true
             message = data.message
         }
     }
 </script>
 
-<main>
-    
-    <form on:submit|preventDefault={submit}>
-        <h1>Sign up</h1>
+
+<div class="grid place-items-center sm:h-4 w-screen">
+    <form on:submit|preventDefault={submit} class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 lg:w-1/4 ">
+      <div class="mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+          Username
+        </label>
+        <input bind:value={username} class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+      </div>
+      <div class="mb-6">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+          Password
+        </label>
+        <input bind:value={password} class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+      </div>
+      <div class="mb-6">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="confirmation">
+            Confirmation
+        </label>
+        <input bind:value = {confirmation} class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="confirmation" type="password" placeholder="******************">
+      </div>
+      <div class="grid place-items-center">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+          Sign up
+        </button>
         <div>
-            <label for="username">Username</label>
-            <input id="username" type="text" bind:value = {username} placeholder="username">
-        </div>
-        <div>
-            <label for="password">Password</label>
-            <input id="password" type="password" bind:value={password} placeholder="Password">
-        </div>
-        <div>
-            <label for="confirmation">Passowrd confirmation</label>
-            <input id="confirmation" type="password" bind:value={confirmation} placeholder="Password">
-        </div>
-        <button type="submit">Submit</button>
+            {#if !error}
+                <h1 class="text-green-600": >{message}</h1>
+            {:else}
+                <h1 class="text-red-600": >{message}</h1>
+            {/if}
+          
+      </div>
+      </div>
     </form>
-
-    <div>
-        <h1>{message}</h1>
-    </div>
-
-</main>
+  </div>
