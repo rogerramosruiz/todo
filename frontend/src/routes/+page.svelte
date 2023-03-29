cf091c1d36eb83e08647da961ef1d972573d03d1
rogerramosruiz <rogerramosruizdev@gmail.com>
2023-03-28 21:13:14 -0400

    show tasks

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import {onMount} from "svelte"
+    import { goto } from '$app/navigation'
+    import {request} from '../lib/request/request'
+    import Task from "../lib/components/Task.svelte";
+
+
+    let tasks = []
+    
+    onMount(async () => {
+        if(localStorage.accessToken == null || localStorage.refreshToken == null){
+            goto('login')
+            return
+        }
+        const resp = await request('api/v1/task', true)
+        if (resp.status === 200)
+            tasks = await resp.json()
+        })
+
+
+
+
+</script>
+
+<main>
+    <h1>Tasks</h1>    
+    {#each tasks as task}
+		<li>
+            <Task task={task}/>
+		</li>
+	{/each}
+
+</main>
