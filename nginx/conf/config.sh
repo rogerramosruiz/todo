8e82e3972882f64a418e9235c110d80a2d07a2d0
rogerramosruiz <rogerramosruizdev@gmail.com>
2023-03-29 21:51:33 -0400

    svelte in docker compose

--- nginx/conf/config.sh
+++ nginx/conf/config.sh
@@ -1,4 +1,4 @@
  #!/bin/bash
 
-envsubst '${AUTH_SERVER},${TASK_SERVER}' < /home/conf/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${AUTH_SERVER},${TASK_SERVER},${FRONTEND_SERVER}' < /home/conf/default.conf > /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
