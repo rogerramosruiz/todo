550c4f1e561fd20dc602e23285d06f7fdc0099db
rogerramosruiz <rogerramosruizdev@gmail.com>
2023-03-31 19:54:19 -0400

    confg.sh edited for windows

--- nginx/conf/config.sh
+++ nginx/conf/config.sh
@@ -1,4 +1,2 @@
- #!/bin/bash
-
-envsubst '${AUTH_SERVER},${TASK_SERVER},${FRONTEND_SERVER}' < /home/conf/default.conf > /etc/nginx/conf.d/default.conf
-nginx -g 'daemon off;'
+#!/bin/bash
+envsubst '${AUTH_SERVER},${TASK_SERVER},${FRONTEND_SERVER}' < /home/conf/default.conf > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
